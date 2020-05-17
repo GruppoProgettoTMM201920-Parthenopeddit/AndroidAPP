230faef36452cf867dc0cc31da352dd11ebde2cd
Marco <marco.sautto@gmail.com>
2020-05-17 02:06:38 +0200

    added some layout in home

--- app/src/main/java/it/uniparthenope/parthenopeddit/android/HomeActivity.kt
+++ app/src/main/java/it/uniparthenope/parthenopeddit/android/HomeActivity.kt
@@ -1,6 +1,7 @@
 package it.uniparthenope.parthenopeddit.android
 
 import android.os.Bundle
+import android.widget.ListView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
@@ -8,6 +9,11 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import it.uniparthenope.parthenopeddit.BasicActivity
 import it.uniparthenope.parthenopeddit.R
+import it.uniparthenope.parthenopeddit.android.adapters.ExpandableListAdapter
+import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
+import kotlin.collections.HashMap
 
 class HomeActivity : BasicActivity() {
 
@@ -26,5 +32,21 @@ class HomeActivity : BasicActivity() {
         ))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        val listHeader = listOf("I tuoi corsi di studio", "I tuoi gruppi")
+
+        val coursesList = listOf("Corso 1","Corso 2","Corso 3","Corso 4")
+        val groupsList = listOf("Gruppo1","Gruppo 2","Gruppo 3","Gruppo 4")
+
+        //val groupList = resources.getStringArray(R.array.groups)
+        
+        
+        val listChild = HashMap<String, List<String>>()
+        listChild.put(listHeader[0], coursesList)
+        listChild.put(listHeader[1], groupsList)
+
+        val expandableListAdapter : ExpandableListAdapter = ExpandableListAdapter(this, listHeader, listChild)
+
+        expandable_list_view.setAdapter(expandableListAdapter)
     }
 }
