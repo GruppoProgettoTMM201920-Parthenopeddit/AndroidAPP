e36a125f1c5c909d855a4b8afc13cc1ffaa5c7e4
Marco <marco.sautto@gmail.com>
2020-05-19 00:46:11 +0200

    addedd username change dialog

--- app/src/main/java/it/uniparthenope/parthenopeddit/android/ui/profile/ProfileFragment.kt
+++ app/src/main/java/it/uniparthenope/parthenopeddit/android/ui/profile/ProfileFragment.kt
@@ -1,20 +1,27 @@
 package it.uniparthenope.parthenopeddit.android.ui.profile
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import it.uniparthenope.parthenopeddit.R
+import kotlinx.android.synthetic.main.change_username_dialog.*
+import kotlinx.android.synthetic.main.change_username_dialog.view.*
+import kotlinx.android.synthetic.main.change_username_dialog.view.error_textview
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 
-//TO-DO: implementare PreferenceFragmentCompat
 class ProfileFragment : Fragment(), PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
 
     private lateinit var profileViewModel: ProfileViewModel
@@ -30,9 +37,27 @@ class ProfileFragment : Fragment(), PreferenceFragmentCompat.OnPreferenceStartFr
 
         profileViewModel.text.observe(viewLifecycleOwner, Observer {
 
-            fab_new_username.setOnClickListener {
-                
-            }
+            fab_new_username.setOnClickListener {val mDialogView = LayoutInflater.from(requireContext()).inflate(R.layout.change_username_dialog, null)
+                //AlertDialogBuilder
+                val mBuilder = AlertDialog.Builder(requireContext())
+                    .setView(mDialogView)
+                    .setTitle("Nuovo username")
+                val  mAlertDialog = mBuilder.show()
+                mDialogView.dialogDoneBtn.setOnClickListener {
+                    val username = mDialogView.dialogUsernameEt.text.toString()
+                    val error_textview = mDialogView.error_textview
+                    if(username.isNotBlank()){
+                        username_textview.setText(username)
+                        mAlertDialog.dismiss()
+                    } else{
+                        error_textview.visibility = View.VISIBLE
+                        Toast.makeText(requireContext(),"ao",Toast.LENGTH_SHORT)
+
+                    }
+                }
+                mDialogView.dialogCancelBtn.setOnClickListener {
+                    mAlertDialog.dismiss()
+                }}
 
         })
 
