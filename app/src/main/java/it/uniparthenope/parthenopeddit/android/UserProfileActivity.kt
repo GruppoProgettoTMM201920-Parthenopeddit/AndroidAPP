9320321cc873663f75edbdb97239adadb0f3df20
Marco <marco.sautto@gmail.com>
2020-06-09 18:51:04 +0200

    other user added

--- app/src/main/java/it/uniparthenope/parthenopeddit/android/UserProfileActivity.kt
+++ app/src/main/java/it/uniparthenope/parthenopeddit/android/UserProfileActivity.kt
@@ -4,9 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import it.uniparthenope.parthenopeddit.BasicActivity
 import it.uniparthenope.parthenopeddit.R
+import it.uniparthenope.parthenopeddit.api.requests.UserRequests
+import it.uniparthenope.parthenopeddit.model.User
 
-class UserProfileActivity : AppCompatActivity() {
+class UserProfileActivity : BasicActivity() {
+
+    private lateinit var user: User
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_profile)
@@ -17,7 +24,12 @@ class UserProfileActivity : AppCompatActivity() {
         val extras = intent.extras
         val id_user = extras?.getString("id_user")
 
-        //TODO: retrieve user from API
+        UserRequests(this, app.auth).getUserByID(id_user!!,{it: User ->
+            user = it
+        },{
+            Toast.makeText(this,"Errore : $it", Toast.LENGTH_LONG).show()
+        })
+
 
         user_activity_button.setOnClickListener {
             val intent = Intent(this, UserActivity()::class.java)
