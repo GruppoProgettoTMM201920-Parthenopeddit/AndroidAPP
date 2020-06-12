4ffdce66eb81c89e3710bc1a9cfb187b324a2eb8
Francesco Bottino <francesco.bottino96@gmail.com>
2020-06-12 17:55:11 +0200

    course posts fragment has infinite scrolling

--- app/src/main/java/it/uniparthenope/parthenopeddit/android/ui/course/post/CoursePostFragment.kt
+++ app/src/main/java/it/uniparthenope/parthenopeddit/android/ui/course/post/CoursePostFragment.kt
@@ -14,9 +14,11 @@ import it.uniparthenope.parthenopeddit.BasicActivity
 import it.uniparthenope.parthenopeddit.R
 import it.uniparthenope.parthenopeddit.android.PostCommentsActivity
 import it.uniparthenope.parthenopeddit.android.UserProfileActivity
+import it.uniparthenope.parthenopeddit.android.adapters.InfiniteScroller
 import it.uniparthenope.parthenopeddit.android.adapters.PostAdapter
 import it.uniparthenope.parthenopeddit.api.requests.CoursesRequests
 import it.uniparthenope.parthenopeddit.api.requests.PostsRequests
+import it.uniparthenope.parthenopeddit.api.requests.UserRequests
 import it.uniparthenope.parthenopeddit.auth.AuthManager
 import it.uniparthenope.parthenopeddit.model.Board
 import it.uniparthenope.parthenopeddit.model.LikeDislikeScore
@@ -26,29 +28,70 @@ import it.uniparthenope.parthenopeddit.util.toGson
 class CoursePostFragment(private var courseID: Int) : Fragment(), PostAdapter.PostItemClickListeners {
 
     private lateinit var auth : AuthManager
-    private lateinit var recycler_view: RecyclerView
 
+    private lateinit var recycler_view: RecyclerView
     private lateinit var postAdapter: PostAdapter
+    private lateinit var layoutManager: LinearLayoutManager
+    private lateinit var infiniteScroller: InfiniteScroller
+    private lateinit var updater: InfiniteScroller.Updater
+
+    private lateinit var transactionStartDateTime: String
+
+    private val per_page = 20
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val root = inflater.inflate(R.layout.fragment_course_post, container, false)
+
+        auth = (activity as BasicActivity).app.auth
 
         recycler_view = root.findViewById(R.id.recycler_view) as RecyclerView
 
         postAdapter = PostAdapter()
         postAdapter.setItemClickListener(this)
         recycler_view.adapter = postAdapter
-        recycler_view.layoutManager = LinearLayoutManager(requireContext())
+
+        layoutManager = LinearLayoutManager(requireContext())
+        recycler_view.layoutManager = layoutManager
         recycler_view.setHasFixedSize(true)
 
-        auth = (activity as BasicActivity).app.auth
+        updater = object : InfiniteScroller.Updater {
+            override fun updateData(pageToLoad: Int, pageSize: Int) {
+                UserRequests(requireContext(), auth).getUserFeed(
+                    page = pageToLoad,
+                    perPage = pageSize,
+                    transactionStartDateTime = transactionStartDateTime,
+                    onSuccess = {
+                        postAdapter.aggiungiPost(it)
+                    },
+                    onEndOfContent = {
+                        infiniteScroller.theresMore = false
+                    },
+                    onFail = {
+                        Toast.makeText(requireContext(), "ERROR IN FETCHING NEW POSTS", Toast.LENGTH_SHORT).show()
+                    }
+                )
+            }
+        }
+        infiniteScroller = InfiniteScroller(
+            layoutManager, updater, per_page
+        )
 
         CoursesRequests(requireContext(), auth).getCoursePosts(
+            page = 1,
+            perPage = per_page,
             course_id = courseID,
             onSuccess = {
                 postAdapter.aggiungiPost(it)
+                if(it.isNotEmpty()) transactionStartDateTime = it[0].timestamp
+                recycler_view.addOnScrollListener(infiniteScroller)
             },
-            onFail = {}
+            onEndOfContent = {
+                //nothing. list is empty.
+            },
+            onFail = {
+                Toast.makeText(requireContext(),it, Toast.LENGTH_LONG).show()
+            }
         )
 
         return root
